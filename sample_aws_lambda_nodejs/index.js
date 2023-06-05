72d0c5ad0f614bf177a49df5d2bc41a68740923b
mikanmarusan <chiakifujimon@yahoo.co.jp>
2023-06-05 21:57:22 +0900

    added sample aws lambda nodejs code

---
+++ sample_aws_lambda_nodejs/index.js
@@ -0,0 +1,39 @@
+
+// Sample Lambda Function
+// 環境変数からBare
+
+const axios = require("axios");
+
+exports.handler = async(event, context) => {
+  
+  const payload = {
+    "daysOfWeek":        [1,2,3,4,5],
+    "genders":           'f',
+    "page":              1
+  };
+  
+  let statusCode   = 200;
+  let responseData = {};
+  
+  await axios({
+    method: 'post',
+    url: 'https://www.example.com/api',
+    headers: {
+      Authorization: `Bearer ${process.env['BEARER_TOKEN']}`,
+      'Content-Type': 'application/json',
+    },
+    data: payload
+  })
+  .then(function (response) {
+    statusCode = response.status;
+    responseData = response.data;
+  })
+  .catch(function (error) {
+    statusCode = error.response.status;
+  });
+
+  return {
+    'statusCode': statusCode,
+    'body': candidateLessons
+  }
+};
